b3b89ce2b60688b454ea824ab99a4dd317413db7
chitti-a7r4 <ukkueditz.uk@gmail.com>
2024-06-06 18:35:08 +0530

    java

---
+++ java/math.java
@@ -0,0 +1,12 @@
+public class math {
+    public static void main(String[] args) {
+        // int x = 1;
+        // int y = 2;
+        // int z = 3;
+        // System.out.println(x + y + z);
+        // this can be written as
+        int x =1,y=2,z=3;
+        System.out.println(x+y+z);
+    }
+    
+}
