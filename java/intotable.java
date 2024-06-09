aecc11557434897361b0d98e32ecc289591d1614
chitti-a7r4 <ukkueditz.uk@gmail.com>
2024-06-09 14:21:17 +0530

    mother

--- java/intotable.java
+++ java/intotable.java
@@ -1,6 +1,6 @@
 public class intotable {
     public static void main(String[] args) {
-        int number = 2;
+        int number = 5;
         for(int i = 1; i <=10 ;i = i+1){
             System.out.println(i*number);
 
