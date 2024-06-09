518cfb5d0a223a5148672bb17206f845690300c1
chitti-a7r4 <ukkueditz.uk@gmail.com>
2024-06-09 21:12:34 +0530

    kukg

---
+++ c/kkkk.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    int j;
+    for ( j = 5; j>=0; j--)
+    {
+        printf("\n");
+        for ( i = 0; i <=j; i++)
+        {
+         printf("*");
+        //  printf("\n");
+
+        }
+        
+    }
+    
+}
---
+++ c/squareprt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    int j;
+    for ( i = 5; i > 0; i--)
+    {
+
+        printf("\n");
+        for ( j = 0; j < 5; j++)
+        {
+         printf("*");
+
+        }
+        
+    }
+    
+}
