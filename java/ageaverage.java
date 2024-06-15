22795bed9e3e3c0327358c3363bf5c8d9ad28ad3
chitti-a7r4 <ukkueditz.uk@gmail.com>
2024-06-15 19:53:05 +0530

    should balance academics

---
+++ java/ageaverage.java
@@ -0,0 +1,15 @@
+public class ageaverage {
+    public static void main(String[] args) {
+        int ages[] = {10,20,30,40,50};
+        float sum;
+        for(int i =0; i <5;i = i+1){
+            sum = ages[i];
+            System.out.println(sum);
+
+        }
+         
+
+    
+}
+    
+}
